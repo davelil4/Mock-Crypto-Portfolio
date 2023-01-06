af4211b2298bb966aa1bc64bb6a9cf2294c495d1
davelil4 <davidlilienfeld4@gmail.com>
2023-01-06 00:29:23 -0500

    Add pandas dataframe to table support

--- app.py
+++ app.py
@@ -54,9 +54,10 @@ app.layout = dbc.Container(children=[
     
     lay.row1,
     lay.row2,
-    lay.row3,
+    lay.buy_form_div,
     dcc.Store(id='coin_mem', storage_type='local'),
-    lay.row4
+    lay.row5,
+    dbc.Row(id="pd_row", children=[])
     
 
     ],
@@ -88,59 +89,119 @@ def display_buy_form(n_clicks):
 
 # Callbacks for table involving storing local data
 
-# Updates coins from data held locally
+# # Updates coins from data held locally
+# @app.callback(
+#     Output("coin_rows", "children"),
+#     Input("coin_mem", "modified_timestamp"),
+#     State("coin_mem", "data")
+# )
+# def coin_data(ts, data):
+#     if ts is None:
+#         raise PreventUpdate
+    
+#     data = data or {}
+#     return data.get('t_rows', None)
+
+# # Updates coin table data
+# @app.callback(
+#     Output("coin_mem", "data"),
+#     [State("reset", "n_clicks"),
+#     Input("buy_submit", "n_clicks"),
+#     State("coins_buy", "value"),
+#     State("price_buy", "value"),
+#     State('coin_mem', 'data')]
+
+# )
+# def update_coins(reset, button, coin, price, data):
+
+#     data = data or {'t_rows': []}
+
+#     if reset>0:
+#         data = None
+#         return data
+
+
+
+#     if button != None and button != 0 and price != None:
+#         new = data['t_rows'].copy()
+#         new.append(html.Tr(id=coin, children=[html.Td(coin), html.Td(price), html.Td("0")]))
+#         data['t_rows'] = new
+#         return data
+
+    
+#     return data
+
+# # Sets reset buy clicks to 0
+# @app.callback(
+#     Output("reset", "n_clicks"),
+#     Input('coin_mem', 'modified_timestamp')
+# )
+# def reset(ts):
+#     if ts is None:
+#         raise PreventUpdate
+#     return 0
+
+# # Adjusts buy clicks to and allows reset to work without adding more coins
+# @app.callback(
+#     Output("buy_submit", "n_clicks"),
+#     [Input('reset', 'n_clicks'),
+#     State("buy_submit", "n_clicks"),
+#     Input("coin_mem", "modified_timestamp")]
+# )
+# def reset_buy(reset, buy, ts):
+#     if reset is None or reset == 0:
+#         raise PreventUpdate
+#     return 0
+
+
+# PANDAS DIFFERENCE
 @app.callback(
-    Output("coin_rows", "children"),
-    Input('coin_mem', 'modified_timestamp'),
-    State('coin_mem', 'data')
+    Output("pd_row", "children"),
+    Input("coin_mem", "modified_timestamp"),
+    State("coin_mem", "data")
 )
-def coin_data(ts, data):
+def pd_data(ts, data):
     if ts is None:
         raise PreventUpdate
-    
     data = data or {}
-    return data.get('t_rows', None)
+    return dbc.Table.from_dataframe(pd.DataFrame(data['df']), dark=False) or []
 
 # Updates coin table data
 @app.callback(
     Output("coin_mem", "data"),
-    [Input("reset", "n_clicks"),
+    [State("reset", "n_clicks"),
     Input("buy_submit", "n_clicks"),
-    Input("coins_buy", "value"),
-    Input("price_buy", "value"),
+    State("coins_buy", "value"),
+    State("price_buy", "value"),
     State('coin_mem', 'data')]
 
 )
-def update_coins(reset, button, coin, price, data):
-    if button is None or reset is None:
-        raise PreventUpdate
+def update_coins_pd(reset, button, coin, price, data):
 
-    data = data or {'t_rows': []}
+    data = {'df': pd.DataFrame({
+        "Coin": [],
+        "Current Price": [],
+        "% start": []
+    }).to_dict("records")}
 
     if reset>0:
         data = None
         return data
 
-    if button != 0 and len(data['t_rows']) != button:
-        new = data['t_rows'].copy()
-        new.append(html.Tr(id=coin, children=[html.Td(coin), html.Td(price), html.Td("0")]))
-        data['t_rows'] = new
+
+
+    if button != None and button != 0 and price != None:
+        new = pd.DataFrame(data['df'])
+        new = pd.concat([pd.DataFrame({
+            "Coin": [coin],
+            "Current Price": [price],
+            "% start": "0"
+        }), new])
+        data['df'] = new.to_dict("records")
         return data
 
     
     return data
-
-# # Resets whole coin memory
-@app.callback(
-    Output("reset", "n_clicks"),
-    Input('coin_mem', 'modified_timestamp')
-)
-def reset(ts):
-    if ts is None:
-        raise PreventUpdate
-    
-    return 0
-
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- layout_helper.py
+++ layout_helper.py
@@ -10,46 +10,67 @@ import api_helper as help
 wallet_head = html.H2('Wallet', className="text-center mb-4"),
 
 # Wallet Buttons
-buy_button = dbc.Col(dbc.Button(id='buy_b', children=['Buy'], color="primary", n_clicks=0))
-sell_button = dbc.Col(dbc.Button(id='sell_b', children=['Sell'], color="primary", n_clicks=0))
-reset_btn = dbc.Col(dbc.Button(id='reset', children=['Reset'], color="primary", n_clicks=0))
-USD_bal = dbc.Col(html.P("10,000.00"))
+buy_button = dbc.Button(id='buy_b', children=['Buy'], color="primary", n_clicks=0)
+sell_button = dbc.Button(id='sell_b', children=['Sell'], color="primary", n_clicks=0)
+reset_btn = dbc.Button(id='reset', children=['Reset'], color="primary", n_clicks=0)
+USD_bal = dbc.InputGroup(
+            [
+                dbc.InputGroupText("$"),
+                dbc.Input(placeholder="Initial Balance", type="number"),
+            ]
+        )
 wallet_bal = dbc.Col(html.P("0"))
 
 
 
 # Building buy form
-buy_coin_dropdown = html.Div(
-    [
-        dbc.Label("Coin", html_for="dropdown"),
-        dcc.Dropdown(
+# buy_coin_dropdown = html.Div(
+    # [
+        # dbc.Label("Coin", html_for="dropdown", width="auto"),
+buy_coin_dropdown = dcc.Dropdown(
             id="coins_buy",
             options=help.optionList,
             value='BTC/USD',
-            clearable=True
+            clearable=True,
+            style={'width': "100px"}
         ),
-    ],
-    className="mb-4",
-)
+    # ],
+    
+# )
 price_buy = dbc.InputGroup(
             [
                 dbc.InputGroupText("$"),
                 dbc.Input(id="price_buy", placeholder="Amount", type="number"),
                 dbc.InputGroupText(".00"),
-            ],
-            className="mb-3",
+            ]
         )
 buy_submit = dbc.Button(id="buy_submit", children="Submit", color="secondary")
-buy_form = dbc.Form(id="buy_form", children=[buy_coin_dropdown, price_buy, buy_submit])
-buy_form_div = html.Div(id="buy_div", children=[buy_form], hidden=True)
+
 
 # Coin table
 coin_table_head = [html.Thead(html.Tr([html.Th("Coin"), html.Th("Current Price ($)"), html.Th("% start")]))]
 coin_table_rows = [html.Tbody(id="coin_rows", children=[])]
 coin_table = dbc.Table(coin_table_head + coin_table_rows, bordered=True, dark=False)
 
+pandasTable = dbc.Table()
+
 # Layout
 row1 = dbc.Row(dbc.Col(wallet_head))
-row2 = dbc.Row([dbc.Col(buy_button, width=1), dbc.Col(sell_button, width=1), dbc.Col(reset_btn, width=1)], className="mb-4")
-row3 = dbc.Row(dbc.Col(buy_form_div), className="mb-4")
-row4 = dbc.Row(dbc.Col(coin_table))
+row2 = dbc.Row(
+    [
+        dbc.Col(buy_button, width=1), 
+        dbc.Col(sell_button, width=1), 
+        dbc.Col(reset_btn, width=1),
+        dbc.Col(USD_bal, width=2)
+    ],
+    className="mb-4")
+row3 = dbc.Row(
+            [
+                dbc.Col(children=buy_coin_dropdown, width="auto", className="me-3"), 
+                dbc.Col(children=price_buy, width="auto", className="me-3"),
+                dbc.Col(children=buy_submit, width="auto")
+            ], 
+            className="g-2"
+        )
+buy_form_div = html.Div(id="buy_div", children=[row3], hidden=True, className="mb-4", style={'width': "auto"})
+row5 = dbc.Row(dbc.Col(coin_table))
